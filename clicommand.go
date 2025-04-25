65910d49708a73d73c9ec3dbf451fcd280ac2c0d
lyroth <earthmage11@gmail.com>
2025-04-24 21:44:29 -0400

    Created scanner  to read input from user; created command struct to store multiple commands; created Help and Exit commands

---
+++ clicommand.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:")
+	fmt.Println("")
+
+	for _, v := range getCommands() {
+		fmt.Printf("%v: %v\n", v.name, v.description)
+	}
+
+	return nil
+}
--- repl.go
+++ repl.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
 
@@ -8,4 +11,44 @@ func cleanInput(text string) []string {
 	lowered_text := strings.ToLower(text)
 	words_list := strings.Fields(lowered_text)
 	return words_list
+}
+
+func replStart() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		input_text := scanner.Text()
+		clean_input := cleanInput(input_text)
+		first_word := clean_input[0]
+
+		command, ok := getCommands()[first_word]
+		if ok {
+			command.callback()
+		} else {
+			fmt.Println("Unknown command")
+		}
+
+	}
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+	}
 }
